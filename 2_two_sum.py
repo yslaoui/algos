bd0fed4acad64b02734cbdb9c501ec0e6d1d13af
yslaoui <yslaouiyassine@gmail.com>
2024-11-01 22:52:18 +0100

    two sum binary search

--- 2_two_sum.py
+++ 2_two_sum.py
@@ -16,7 +16,7 @@
 # Input: nums = [3,3], target = 6
 # Output: [0,1]
 
-
+import math
 
 def twoSumBruteForce(nums, target): 
     # time: O(n2) space: O(1)
@@ -37,6 +37,7 @@ def twoSumDictionary(nums, target):
     return -1    
 
 def twoSumOnSortedList(nums, target):
+    # O(n), O(1)
     L = 0
     R = len(nums) - 1
     while (L<R):
@@ -48,7 +49,32 @@ def twoSumOnSortedList(nums, target):
             return [L,R]
     return -1
 
+ 
+def twoSumSortedListBinarySearch(nums, target):
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        search = bsearch(nums, complement, i+1, len(nums)-1)
+        if search != -1:
+            return [i, search]  
+    return -1   
 
+def bsearch(arr, searched, start, end):
+    L = start
+    R = end
+    while (L <= R):
+        mid = (L+R)//2
+        if (searched > arr[mid]):
+            L = mid+1
+        if (searched < arr[mid]):
+            R = mid-1
+        if (searched == arr[mid]):
+            return mid
+    return -1
+
+        
+ 
+ 
+# TODO binary search on sorted list
 
 
 
@@ -56,6 +82,8 @@ def twoSumOnSortedList(nums, target):
 nums1 = [2,7,11,15]
 target1 = 9
 # [0,1]
+
+
 
 nums2 = [3,2,4]
 target2 = 6
@@ -65,5 +93,5 @@ nums3 = [3,3]
 target3 = 6
 # [0,1]
 
-result = twoSumOnSortedList(nums1, target1)
+result = twoSumSortedListBinarySearch(nums1, target1)
 print(result)
